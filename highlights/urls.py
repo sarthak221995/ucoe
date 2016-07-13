cc7ee4b7e9a96447cf12813bb8e7c73c9f3ceef1
sarthak22 <sarthak221995@gmail.com>
2016-07-13 13:17:18 +0530

    icrtc_2

--- highlights/urls.py
+++ highlights/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^1/$', views.post_list, name='post_list'),
     url(r'^2/$', views.post_icrtc, name='post_icrtc'),
+    url(r'^3/$', views.post_call, name='post_call'),
+    url(r'^4/$', views.post_gta, name='post_gta'),
+    url(r'^5/$', views.post_date, name='post_date'),
+    url(r'^6/$', views.post_contact, name='post_contact'),
+   
 ]
--- highlights/views.py
+++ highlights/views.py
@@ -6,4 +6,20 @@ def post_list(request):
 
 def post_icrtc(request):
     return render(request, 'highlights/icrtc.html', {})
+
+def post_date(request):
+    return render(request, 'highlights/date.html', {})
+
+def post_call(request):
+    return render(request, 'highlights/call.html', {})
+
+
+def post_gta(request):
+    return render(request, 'highlights/gta.html', {})
+
+
+def post_contact(request):
+    return render(request, 'highlights/contact.html', {})
+
+
     # Create your views here.
